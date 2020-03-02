9673541a11f6f9c5cfe90aa2fb0e76df58f05188
Kadin Eastway <keastway@Kadins-MacBook-Air.local>
2020-03-01 20:40:01 -0500

    Applied recommended changes.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,26 @@
 import { confirmInput, log, selectOption } from 'app/functions';
 
 export async function main() {
-	const board = ["", "", "", "O", "X", "O", "X", "O", "X"];
-	let boardDisplay: any = "";
-	/** goes through every value in the board, and sets it equal to a string with dashes and spaces as needed */
-	function displayBoard(board: any) {
-		for(let i = 0; i < 9; i++) {
-			let whiteSpace: string;;
-			if(i > 7) {
-				whiteSpace = "";
-			}
-			else if((i + 1) % 3 === 0) {
-				whiteSpace = "\n–– ––– ––\n";
-			} else {
-				whiteSpace = " | ";
-			}
-			const positionValue: any = (board[i] === "") ? " " : board[i] ;
-			boardDisplay = boardDisplay + positionValue + whiteSpace;
-		}
-
-		log(boardDisplay);
-	}
+	const board = ['', '', '', 'O', 'X', 'O', 'X', 'O', 'X'];
 
 	displayBoard(board);
+}
+
+	/** goes through every value in the board, and sets it equal to a string with dashes and spaces as needed */
+function displayBoard(board: any) {
+	let boardDisplay: any = '';
+	for(let i = 0; i < 9; i++) {
+		let whiteSpace: string;
+	if(i > 7) {
+		whiteSpace = '';
+	}
+	else if((i + 1) % 3 === 0) {
+		whiteSpace = '\n–– ––– ––\n';
+	} else {
+		whiteSpace = ' | ';
+	}
+	const positionValue: any = (board[i] === '') ? ' ' : board[i];
+	boardDisplay = boardDisplay + positionValue + whiteSpace;
+	}
+	log(boardDisplay);
 }
